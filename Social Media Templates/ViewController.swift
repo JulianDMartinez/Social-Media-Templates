b51089a62da43d68d945f03cc13ac7c88910c791
Julian Martinez <julian.martinez.s@outlook.com>
2021-05-21 20:29:09 -0500

    Initial Commit

---
+++ Social Media Templates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Media Templates
+//
+//  Created by Julian Martinez on 5/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
